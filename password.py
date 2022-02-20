f535fa3408aa36f9dbfae423311deabfe32bfb87
Yuzhe Deng <g0922884270@gmail.com>
2022-02-20 09:01:09 +0800

    password_2

--- password.py
+++ password.py
@@ -1,13 +1,15 @@
 password = 'a123456'
-x=2
+x=3
 
-while True :
+while x > 0 :
+	x = x - 1
 	ans = input('請輸入密碼:')
 	if ans == password :
 		print('登入成功')
 		break
-	if ans != password :
-		print('"密碼錯誤 還有',x,'次機會')
-		x=x-1
-		if x == 0 :
-			break
+	else :
+		print('密碼錯誤')
+		if x > 0:
+			print('還有',x,'次機會')
+		else:
+			print('鎖帳號 恭喜')
